1f6f73d9e405ff7948624b6bcf386f6d40a69abb
RobertLP <robert-popijus@hotmail.com>
2016-01-12 22:59:50 +0100

    Blouwout

---
+++ BlowoutPlayer/BlowGun.cs
@@ -0,0 +1,372 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BlowGun : MonoBehaviour 
+{
+	public Texture oxygenBarText;
+	[Header("Audio")]
+	public float soundFlux = 0;
+	public int randomAudioFluxChance = 1;
+	public float soundVolume = 1F;
+	public float addRemoveSoundSpeed = 0.1F;
+	[Space(10)]
+	public float pushMaxSoundPitchAndPowerMultiplier = 1F;
+	public float suckMaxSoundPitchAndPowerMultiplier = 1F;
+	[Space(10)]
+	public AudioClip soundEffect;
+	public AudioClip soundGunPush;
+	public AudioClip soundGunSuck;
+	private int randomAudioNumberGen;
+	private AudioSource gunEffect;
+	private AudioSource pushAudioSource;
+	private AudioSource suckAudioSource;
+	private GameObject pushSoundObject;
+	private GameObject suckSoundObject;
+
+	[Header("Oxygen")]
+	public float maxOgygen = 100F;
+	public float oxygenCost = 0.2F;
+	public float oxygenSuck = 0.2F;
+	[HideInInspector]public float oxygen;
+
+	[Header("Gun Modes")]
+	public bool linearPull = false;
+	public float linearPullMultiplier = 8F; // temp var
+	public enum GunMode{
+		suck,blow
+	}
+	public GunMode gunMode;
+
+	[Header("Gun Properties")]
+	public float pullAttractForce = 1F;
+	public float sidewardPullForce = 0.5F;
+	[Space(10)]
+	public float frontalBlowForce = 1F;
+	public float sidewardBlowForce = 0.5F;
+	[Space(10)]
+	public float sidewardThreshold = 0.5F;
+	public float gunRange = 10F;
+
+	[HideInInspector]public bool GunInUse = false;
+
+	[Header("GameObjects")]
+	public GameObject interActiveObject;
+	public List <GameObject> InRangeObjects;
+	public List <GameObject> LoadedTargets;
+	public TargetFind[] foundObjects;
+	[System.Serializable]
+	public class TargetFind{
+		public string TagName;
+	}
+
+	public Camera cam;
+
+	void Awake()
+	{
+		cam = GameObject.FindGameObjectWithTag("MainCam").GetComponent<Camera>();
+		CreateAudio();
+		SetUpLists();
+		oxygen = maxOgygen;
+	}
+
+	void SetUpLists()
+	{
+		InRangeObjects = new List<GameObject>();
+		LoadedTargets = new List<GameObject>();
+		foreach(TargetFind tf in foundObjects)
+		{      
+			GameObject[] TempLoadedTargets = GameObject.FindGameObjectsWithTag(tf.TagName);
+			foreach(GameObject loadedTarget in TempLoadedTargets)
+			{
+				LoadedTargets.Add(loadedTarget);
+			}
+		}
+	}
+
+	void CreateAudio()
+	{
+		pushSoundObject = new GameObject();
+		suckSoundObject = new GameObject();
+		pushSoundObject.transform.position = this.transform.position;
+		suckSoundObject.transform.position = this.transform.position;
+		pushSoundObject.name = "PushSoundObject";
+		suckSoundObject.name = "SuckSoundObject";
+		pushSoundObject.transform.parent = this.transform;
+		suckSoundObject.transform.parent = this.transform;
+		gunEffect = gameObject.AddComponent<AudioSource>();
+		pushSoundObject.AddComponent<AudioSource>();
+		suckSoundObject.AddComponent<AudioSource>();
+		pushAudioSource = pushSoundObject.GetComponent<AudioSource>();
+		suckAudioSource = suckSoundObject.GetComponent<AudioSource>();
+		pushAudioSource.spatialBlend = 1;
+		suckAudioSource.spatialBlend = 1;
+		gunEffect.spatialBlend = 1;
+		gunEffect.clip = soundEffect;
+		pushAudioSource.clip = soundGunPush;
+		suckAudioSource.clip = soundGunSuck;
+		pushAudioSource.loop = true;
+		suckAudioSource.loop = true;
+		pushAudioSource.pitch = 0F;
+		suckAudioSource.pitch = 0F;
+		gunEffect.volume = 0.8F;
+		pushAudioSource.volume = 0F;
+		suckAudioSource.volume = 0F;
+	}
+
+	void Update()
+	{
+		// TEMP
+		if(Input.GetKeyDown(KeyCode.E) && gunMode == GunMode.suck)
+		{
+			gunMode = GunMode.blow;
+			if(!gunEffect.isPlaying)gunEffect.Play();
+		}
+		else if (Input.GetKeyDown(KeyCode.E) && gunMode == GunMode.blow) 
+		{
+			gunMode = GunMode.suck;
+			if(!gunEffect.isPlaying)gunEffect.Play();
+		}
+		// TEMP
+		if(Input.GetMouseButton(0))
+		{
+			ActivateGun();
+			GunInUse = true;
+		}
+		else 
+			GunInUse = false;
+		SilenceSound();
+		if(oxygen < 0) 
+			oxygen = 0;
+		if(!GunInUse) 
+			ResetSpecialTargetValues();
+	}
+
+	void ActivateGun()
+	{
+		randomAudioNumberGen = Mathf.RoundToInt(Random.Range(0,randomAudioFluxChance));
+		GetTargets();
+		CalcTargets();
+		OxygenLoss();
+		GenerateSound();
+	}
+
+	void GetTargets()
+	{
+		foreach(GameObject loadedTarget in LoadedTargets)
+		{
+			float targetDistance = Vector3.Distance(loadedTarget.transform.position, this.transform.position);
+			if(targetDistance <= gunRange && !InRangeObjects.Contains(loadedTarget))
+			{
+				InRangeObjects.Add(loadedTarget);
+			}
+			else if(targetDistance > gunRange && InRangeObjects.Contains(loadedTarget))
+			{
+				InRangeObjects.Remove(loadedTarget);
+			}
+		}
+	}
+
+	void CalcTargets()
+	{
+		foreach(GameObject inRangeObject in InRangeObjects) // x is offset
+		{
+			if(this.transform.InverseTransformPoint(inRangeObject.transform.position).z > 0)
+			{
+				float dist = Vector3.Distance(inRangeObject.transform.position, this.transform.position);
+				Vector3 relativeTargetPosition = this.transform.InverseTransformPoint(inRangeObject.transform.position);
+				relativeTargetPosition.y = 0;
+				relativeTargetPosition.z = 0;
+
+				if(inRangeObject.tag == "Propellor" || inRangeObject.tag == "OxygenPlant" || inRangeObject.tag == "GeneratorTumbleGreed")
+				{
+					SpecialTargets(inRangeObject, relativeTargetPosition.x, dist, relativeTargetPosition.z);
+				}
+				else if(gunMode == GunMode.suck) 
+					ManipTargetPull(dist, relativeTargetPosition.x, inRangeObject);
+				else if(gunMode == GunMode.blow && oxygen > 0) 
+					ManipTargetPush(dist, relativeTargetPosition.x, inRangeObject);
+			}
+		}
+	}
+
+	void SpecialTargets(GameObject target, float offset, float distance, float frontCheck)
+	{
+		if(target.tag == "OxygenPlant")
+		{
+			if(gunMode == GunMode.suck)
+				RetrieveOxygen(target,offset,distance);
+			else if(gunMode == GunMode.blow)
+				InsertOxygen(target,offset,distance);
+		}
+		else if(target.tag == "Propellor")
+		{
+			interActiveObject = GameObject.FindGameObjectWithTag("Elevator");
+			float MaxMoveUpSpeed = interActiveObject.GetComponent<Elevator>().maxMoveUpSpeed;
+			interActiveObject.GetComponent<Elevator>().frontCheck = frontCheck;
+
+			if(gunMode == GunMode.blow && oxygen > 0)
+			{
+				float gunValue = ((gunRange-distance)*1.4f )*0.01f;
+				interActiveObject.GetComponent<Elevator>().elevatorSpeed += gunValue;
+				interActiveObject.GetComponent<Elevator>().currentSpeedInput = gunValue;
+			}
+		}
+		else if(target.tag == "GeneratorTumbleGreed")
+		{
+			interActiveObject = target;
+			if(!interActiveObject.GetComponent<TumbleGreedInGenerator>().fleeBack)
+			{
+				if(gunMode == GunMode.suck) 
+					ManipTargetPull(distance, offset, target);
+				else if(gunMode == GunMode.blow && oxygen > 0) 
+					ManipTargetPush(distance, offset, target);
+			}
+			interActiveObject.GetComponent<TumbleGreedInGenerator>().beingSuckedOut = true;
+
+		}
+	}
+
+	void ResetSpecialTargetValues()
+	{
+		if(interActiveObject != null && interActiveObject.tag == "Elevator" && interActiveObject.GetComponent<Elevator>().currentSpeedInput != 0)
+		{
+			interActiveObject.GetComponent<Elevator>().currentSpeedInput = 0;
+
+		}
+		else if(interActiveObject != null && interActiveObject.tag == "GeneratorTumbleGreed")
+			interActiveObject.GetComponent<TumbleGreedInGenerator>().beingSuckedOut = false;
+	}
+
+	void ManipTargetPush(float distance, float offset, GameObject target)
+	{
+		if(offset > -sidewardThreshold && offset < sidewardThreshold){
+			Rigidbody rb = null;
+			if(rb == null) rb = target.GetComponent<Rigidbody>();
+
+			float frontalForce = (gunRange-distance) * frontalBlowForce;
+			float sidewardForce = (offset*sidewardBlowForce) * (gunRange-distance);//sidewardBlowForce;
+
+			Vector3 PlayerDirectionFrontal = this.transform.TransformDirection(Vector3.forward * (frontalForce * pushAudioSource.pitch));
+			Vector3 PlayerDirectionSideward = this.transform.TransformDirection(Vector3.right * (sidewardForce * pushAudioSource.pitch));
+			Vector3 PlayerDirectionCalc = PlayerDirectionFrontal + PlayerDirectionSideward;
+
+			rb.AddForce(PlayerDirectionCalc);
+		}
+	}
+
+	void ManipTargetPull(float distance, float offset, GameObject target)
+	{
+		if(linearPull)
+			target.transform.position = Vector3.Lerp(target.transform.position, this.transform.position, (pullAttractForce/1000)*((gunRange-distance)*linearPullMultiplier) );
+		else if(offset > -sidewardThreshold && offset < sidewardThreshold)
+		{
+			Rigidbody rb = null;
+			if(rb == null) rb = target.GetComponent<Rigidbody>();
+
+			float pullForce = (gunRange-distance) * pullAttractForce;
+			float pullSideForce = (offset*sidewardPullForce) * (gunRange-distance);
+
+			Vector3 PlayerDirectionBackward = this.transform.TransformDirection(Vector3.back * (pullForce * suckAudioSource.pitch));
+			Vector3 PlayerDirectionInward = this.transform.TransformDirection(Vector3.left * (pullSideForce * suckAudioSource.pitch));
+			Vector3 PlayerDirectionPullCalc = PlayerDirectionBackward + PlayerDirectionInward;
+
+			rb.AddForce(PlayerDirectionPullCalc);
+		}
+	}
+
+	void OxygenLoss()
+	{
+		if(gunMode == GunMode.blow && oxygen > 0) oxygen -= oxygenCost;
+	}
+
+	void RetrieveOxygen(GameObject selecTarget, float offset, float distance)
+	{
+		if(oxygen < maxOgygen)
+		{
+			float finalkOxSuck = oxygenSuck;
+			selecTarget.GetComponent<Plant>().oxygenHolding -= finalkOxSuck;
+			oxygen += finalkOxSuck;
+		}
+	}
+
+	void InsertOxygen(GameObject selecTarget, float offset, float distance)
+	{
+		if(oxygen > 0 && selecTarget.GetComponent<Plant>().oxygenHolding < selecTarget.GetComponent<Plant>().oxygenStorage)
+		{
+			float finalkOxSuck = oxygenSuck;
+			selecTarget.GetComponent<Plant>().oxygenHolding += finalkOxSuck;
+			oxygen -= finalkOxSuck;
+		}
+	}
+
+	void GenerateSound()
+	{
+		if(oxygen > 0 && gunMode == GunMode.blow)
+		{
+			if(!pushAudioSource.isPlaying) pushAudioSource.Play();
+			if(pushAudioSource.pitch < pushMaxSoundPitchAndPowerMultiplier) pushAudioSource.pitch += addRemoveSoundSpeed;
+			if(pushAudioSource.volume < soundVolume) pushAudioSource.volume += addRemoveSoundSpeed;
+			if(pushAudioSource.volume >= soundVolume && pushAudioSource.pitch >= pushMaxSoundPitchAndPowerMultiplier && randomAudioNumberGen == 0)
+				pushAudioSource.pitch = Mathf.Lerp(pushAudioSource.pitch, pushMaxSoundPitchAndPowerMultiplier + Random.Range(0F,soundFlux), 0.1F);
+		}
+		if(gunMode == GunMode.suck)
+		{
+			if(!suckAudioSource.isPlaying) suckAudioSource.Play();
+			if(suckAudioSource.pitch < suckMaxSoundPitchAndPowerMultiplier) suckAudioSource.pitch += addRemoveSoundSpeed;
+			if(suckAudioSource.volume < soundVolume) suckAudioSource.volume += addRemoveSoundSpeed;
+			if(suckAudioSource.volume >= soundVolume && suckAudioSource.pitch >= suckMaxSoundPitchAndPowerMultiplier && randomAudioNumberGen == 0)
+				suckAudioSource.pitch = Mathf.Lerp(suckAudioSource.pitch, suckMaxSoundPitchAndPowerMultiplier + Random.Range(0F,soundFlux), 0.1F);
+		}
+	}
+
+	void SilenceSound()
+	{
+		if(gunMode == GunMode.suck || oxygen <= 0)
+		{
+			if(pushAudioSource.pitch > 0) pushAudioSource.pitch -= addRemoveSoundSpeed;
+			if(pushAudioSource.volume > 0) pushAudioSource.volume -= addRemoveSoundSpeed;
+			if(pushAudioSource.pitch <= 0 && pushAudioSource.volume <=0 )
+			{
+				pushAudioSource.pitch = 0;
+				pushAudioSource.Stop();
+			}
+		}
+		else if(gunMode == GunMode.blow)
+		{
+			if(suckAudioSource.pitch > 0) suckAudioSource.pitch -= addRemoveSoundSpeed;
+			if(suckAudioSource.volume > 0) suckAudioSource.volume -= addRemoveSoundSpeed;
+			if(suckAudioSource.pitch <= 0 && suckAudioSource.volume <=0 )
+			{
+				suckAudioSource.pitch = 0;
+				suckAudioSource.Stop();
+			}
+		}
+		if(!GunInUse)
+		{
+			if(pushAudioSource.pitch > 0) pushAudioSource.pitch -= addRemoveSoundSpeed*(pushAudioSource.pitch);
+			if(pushAudioSource.volume > 0) pushAudioSource.volume -= addRemoveSoundSpeed;
+			if(suckAudioSource.pitch > 0) suckAudioSource.pitch -= addRemoveSoundSpeed*(suckAudioSource.pitch);
+			if(suckAudioSource.volume > 0) suckAudioSource.volume -= addRemoveSoundSpeed;
+
+			if(pushAudioSource.pitch <= 0 && pushAudioSource.volume <=0 )
+			{
+				pushAudioSource.pitch = 0;
+				pushAudioSource.Stop();
+			}
+			if(suckAudioSource.pitch <= 0 && suckAudioSource.volume <=0 )
+			{
+				suckAudioSource.pitch = 0;
+				suckAudioSource.Stop();
+			}
+		}
+	}
+
+	public void OnGUI()
+	{
+		Vector3 OxygenBarSize = cam.ViewportToScreenPoint(new Vector3(0.01f,0.03f,0));
+		Rect oxygenBar = new Rect((Screen.width)-OxygenBarSize.x, 10 ,(-oxygen*2), OxygenBarSize.y);
+		UnityEngine.GUI.DrawTexture(oxygenBar, oxygenBarText,ScaleMode.StretchToFill);
+	}
+
+}
---
+++ BlowoutPlayer/cowboyController.cs
@@ -0,0 +1,210 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cowboyController : MonoBehaviour {
+	
+	[Header("Movement")]
+	public float velocityh;
+	public float velocityv;
+	public float acceleration = 1.5f;
+	public float friction = 10f;
+
+	public float inAirSpeed = 3f;
+	public float airSlowdown = 0.1f;
+	[Range(0.1f, 1f)][SerializeField] public float maxSpeed = 0.1f;
+	[Range(1f, 100f)][SerializeField] public float gravityStrength = 2f;
+	private Vector3 movement;
+	private Vector3 jumpMovement;
+	private RaycastHit floorHit;
+	
+	[Range(1f, 10f)][SerializeField]public float jumpStrength = 12f;
+	public bool isGrounded;
+	public GameObject standingOnObject;
+	public float terrainSlope;
+	
+	private Animator anim;                      
+	private Rigidbody playerRigidbody;
+	private int floorMask;                      
+	private float camRayLength = 100f;
+	private Camera mainCamera;
+
+	private GameObject OxGun;
+	[Header("Debug")]
+	public bool visibleRange = false;
+
+	LineRenderer lineRenderer;
+	public Material lineMat;
+
+	[Header("Camera")]
+	public GameObject CameraBase;
+
+	[Header("CharacterModel")]
+	public GameObject characterModel;
+	private Animator aniMat;
+
+	public bool killed;
+
+	
+	void Awake ()
+	{
+		aniMat = characterModel.GetComponent<Animator>();
+		CameraBase = GameObject.Find("CameraBase");
+		OxGun = GameObject.Find("Gun");
+		lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.material = lineMat;
+		lineRenderer.SetWidth(0.2F, 0.2F);
+		//lineMat.renderQueue = 9999999;
+		// NEW ABOVE
+		floorMask = LayerMask.GetMask ("FloorMask");
+		anim = GetComponent <Animator> ();
+		playerRigidbody = GetComponent <Rigidbody> ();
+		playerRigidbody.mass = 100f;
+		mainCamera = GameObject.Find("Camera").GetComponent<Camera>();
+		GameObject[] FoundTerrain = GameObject.FindGameObjectsWithTag("Terrain");
+
+		// turn of bounciness?
+		foreach(GameObject terrainPiece in FoundTerrain)
+		{
+			terrainPiece.layer = 8; // 8 is floormask
+			terrainPiece.GetComponent<Collider>().material.bounciness = 0f;
+		}
+		this.GetComponent<Collider>().material.bounciness = 0f;
+	}
+		
+	void Update()
+	{
+		if(visibleRange) 
+			RangeLine(); 
+		else 
+			lineRenderer.enabled = false;
+
+	}
+
+	void AnimationsFunc(float h, float v)
+	{
+		aniMat.SetInteger("WalkHorizontal", Mathf.RoundToInt(h));
+		aniMat.SetInteger("WalkVertical", Mathf.RoundToInt(v));
+		aniMat.SetFloat("WalkHorizontalF", h);
+		aniMat.SetFloat("WalkVerticalF",v);
+	}
+		
+	void RangeLine()
+	{
+		//print(floorHit.point);
+		float lineRange = Vector3.Distance(this.transform.position, floorHit.point);
+		if(lineRange <= OxGun.GetComponent<BlowGun>().gunRange )
+		{
+			lineRenderer.enabled = true;
+			lineRenderer.SetPosition(0,new Vector3(this.transform.position.x,this.transform.position.y-(this.transform.localScale.y),this.transform.position.z));
+			lineRenderer.SetPosition(1,floorHit.point);
+			//centerPt + Vector3.ClampMagnitude(offset, radius);
+
+		}
+		else
+		{
+			lineRenderer.enabled = false;
+		}
+
+	}
+	
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		//print ("Horizontal: " + h + " Vertical: "+ v);
+		AnimationsFunc(h,v);
+		Move(h, v);
+		Turning();
+		CheckAngleTerrain(h, v);
+		//Animating (h, v);
+	}
+	
+	void Move (float h, float v)
+	{
+		if(this.transform.parent != null && !isGrounded)
+			this.transform.parent = null;
+
+		velocityh += acceleration * h * Time.deltaTime;
+		velocityh -= velocityh * friction * Time.deltaTime;
+		velocityv += acceleration * v * Time.deltaTime;
+		velocityv -= velocityv * friction * Time.deltaTime;
+
+		Vector3 newMovement = this.transform.TransformDirection(velocityh,0,velocityv);
+		movement.Set(newMovement.x, 0, newMovement.z);
+
+		if(!isGrounded)
+			playerRigidbody.MovePosition (transform.position + movement);
+		else if(isGrounded && standingOnObject.name != "elevatorFrame")
+		{
+			playerRigidbody.MovePosition (transform.position + (movement * terrainSlope));
+			if(CameraBase.GetComponent<CameraFollow>().onElevator)
+				CameraBase.GetComponent<CameraFollow>().onElevator = false;
+		}
+		else if(isGrounded && standingOnObject.name == "elevatorFrame") 
+			ElevatorStandMovement(movement);
+	}
+
+	void ElevatorStandMovement(Vector3 movement)
+	{
+		CameraBase.GetComponent<CameraFollow>().onElevator = true;
+		playerRigidbody.MovePosition (transform.position + movement);
+		if(this.transform.parent != standingOnObject.transform) 
+			this.transform.SetParent(standingOnObject.transform);
+	}
+
+	void HandleAirborneMovement()
+	{
+		Vector3 extraGravityForce = (Physics.gravity * gravityStrength) - Physics.gravity;
+		playerRigidbody.AddForce(extraGravityForce);
+	}
+	
+	void Turning ()
+	{
+		this.transform.Rotate(new Vector3(0,Input.GetAxis("Mouse X"),0));
+	}
+	
+	void CheckAngleTerrain(float h, float v)
+	{
+		if(( h>0 || v>0)){
+			RaycastHit hit;
+			if(Physics.SphereCast(new Vector3(this.transform.position.x, this.transform.position.y+(this.transform.localScale.y/2), this.transform.position.z), this.transform.localScale.x, Vector3.down,out hit, transform.localScale.y))
+			{
+				standingOnObject = hit.transform.gameObject;
+			}
+			terrainSlope = hit.normal.y;
+		}
+	}
+	
+
+	void Animating (float h, float v)
+	{
+		// Create a boolean that is true if either of the input axes is non-zero.
+		bool walking = h != 0f || v != 0f;
+		
+		// Tell the animator whether or not the player is walking.
+		anim.SetBool ("IsWalking", walking);
+	}
+	
+	void OnCollisionStay (Collision collisionInfo)
+	{
+		if(collisionInfo.gameObject == standingOnObject) isGrounded = true;
+	}
+	
+	void OnCollisionExit (Collision collisionInfo)
+	{
+		if(collisionInfo.gameObject == standingOnObject) isGrounded = false;
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("killer"))
+		{
+			if(!killed)
+			{
+				GameObject.Find("QuestCam").GetComponent<QuestHandler>().StartSceneExit();
+				killed = true;
+			}
+		}
+	}
+}
+
